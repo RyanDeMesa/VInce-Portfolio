d54ddbfa167ae232d007d53b212d110da84a039c
Ryan De Mesa <ryandemesa09@gmail.com>
2023-07-12 14:17:21 -0600

    Fix img folder & add styling

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,23 +3,49 @@ import React from "react";
 //   return <img src={Image1}></img>;
 
 const Gallery = () => {
-  const imageList = [...new Array(30)].map(
+  const imageList = [...new Array(29)].map(
     (number, index) => `/img/photo${index + 1}.jpg`
   );
 
   return (
-    <ul className="img-grid">
+    <ul className="grid grid-cols-3 gap-4">
       {imageList.map((imgUrl, index) => {
         return (
-          <li className="item">
-            <figure>
-              <img src={imgUrl} alt="collection" />
+          <li className="flex items-center justify-center">
+            <figure className="p-2">
+              <img
+                src={imgUrl}
+                alt="collection"
+                className="w-full h-full object-cover rounded"
+              />
             </figure>
           </li>
         );
       })}
     </ul>
   );
+
+  //   return (
+  //     <div clas="flex">
+  //       <div className="grid grid-cols-3 gap-2">
+  //         {imageList && imageList.map((imageUrl) => <img src={imageUrl} />)}
+  //       </div>
+  //     </div>
+  //   );
+
+  //   return (
+  //     <ul className="img-grid">
+  //       {imageList.map((imgUrl, index) => {
+  //         return (
+  //           <li className="item">
+  //             <figure>
+  //               <img src={imgUrl} alt="collection" />
+  //             </figure>
+  //           </li>
+  //         );
+  //       })}
+  //     </ul>
+  //   );
 };
 
 export default Gallery;
