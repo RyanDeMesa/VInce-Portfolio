d1b78ed0221eefc25494dbb9f1869e1901e340be
Ryan De Mesa <ryandemesa09@gmail.com>
2023-07-12 14:18:18 -0600

    Remove comments

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,6 +1,4 @@
 import React from "react";
-// import Image1 from "../img/photo1.jpg";
-//   return <img src={Image1}></img>;
 
 const Gallery = () => {
   const imageList = [...new Array(29)].map(
@@ -24,28 +22,6 @@ const Gallery = () => {
       })}
     </ul>
   );
-
-  //   return (
-  //     <div clas="flex">
-  //       <div className="grid grid-cols-3 gap-2">
-  //         {imageList && imageList.map((imageUrl) => <img src={imageUrl} />)}
-  //       </div>
-  //     </div>
-  //   );
-
-  //   return (
-  //     <ul className="img-grid">
-  //       {imageList.map((imgUrl, index) => {
-  //         return (
-  //           <li className="item">
-  //             <figure>
-  //               <img src={imgUrl} alt="collection" />
-  //             </figure>
-  //           </li>
-  //         );
-  //       })}
-  //     </ul>
-  //   );
 };
 
 export default Gallery;
